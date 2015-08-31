8e6ddeebd78298f4afdb7f92d56fc5734ec29350
dynamoelectric <dynamoelectric@a2plcpnl0322.prod.iad2.secureserver.net>
2015-08-30 20:21:51 -0700

    Coming soon

--- index.php
+++ index.php
@@ -5,7 +5,37 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Dynamoelectricinc Home page</title>
 
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Coming Soon!</h1>
+    <div class="col-md-9 col-md-push-3">
+        <div class="col-md-3 col-md-pull-9">
+            <span style="background-color:activecaption;"></span>
+        </div>   
+    </div>    
 
-echo "Comming Soon! 08/31/2015";
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
